a08f7bb978f1dec0202dcdf56c2c6d661217432e
HKnudsen <henrik@csolutions.no>
2020-05-22 01:10:59 +0200

    lagt inn mulighet for mail

--- javascript/taskPageScript.js
+++ javascript/taskPageScript.js
@@ -11,17 +11,28 @@ function submitModal(e){
     const lastName = document.querySelector("[name='lname']").value;
     const email = document.querySelector("[name='email']").value;
 
+    const listOfUsers = JSON.parse(window.localStorage.getItem('user')) || [];
+
     const participant = {firstName, lastName, email};
     console.log(participant);
-    window.localStorage.setItem('user', JSON.stringify(participant));
+    listOfUsers.push(participant);
+    window.localStorage.setItem('user', JSON.stringify(listOfUsers));
 
     closeParticipantModal();
 }
 
 function renderParticipants(){
     participants = JSON.parse(window.localStorage.getItem('user')) || [];
-
     const memberList = document.getElementById("project-members");
-    memberList.innerHTML += participants.firstName;
+    //Printing all names to the screen for each participant in localStorage using the forEach methord with an arrow function
+    participants.forEach(participant => {
+        console.log(participant.firstName);
+        memberList.innerHTML += `
+            <div><p>${participant.firstName} ${participant.lastName} ${participant.email}</p></div>
+        `;
+    })
+    
+   
 
 }
+ 
