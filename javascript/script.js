ca6e778a1dab39bb3b8dbd716242518e25cce27f
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-05-22 20:28:43 +0200

    Lagt inn elementer i localstorage

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,8 @@ var projectContainer = document.getElementById("projects-container");
 var newProject = document.getElementById("new-project-modal");
 var openModal = document.getElementById("open-modal");
 var exitModal = document.getElementById("exit-modal");
-var input = document.getElementById("modal-input");
+var modalNameinput = document.getElementById("modal-name-input");
+var modalDateinput = document.getElementById("modal-date-input");
 
 var createProjectBtn = document.getElementById("create-project-btn");
 
@@ -13,82 +14,108 @@ var createProjectBtn = document.getElementById("create-project-btn");
 
 
 /* On clicks */
-openModal.onclick = showModal;  
+openModal.onclick = showModal;
 exitModal.onclick = hideModal;
 createProjectBtn.onclick = createProject;
 
-   
+
 /* Function that makes the "create procject" window pop up*/
-function showModal(){
+function showModal() {
     newProject.style.visibility = "visible";
     newProject.style.opacity = 1;
- }
+}
 
 /* Function that makes the "create project" window shut down*/
- function hideModal(){
+function hideModal() {
     newProject.style.visibility = "hidden";
-    newProject.style.opacity = 0;  
- }
+    newProject.style.opacity = 0;
+}
 
 
 
 
 window.onload = init;
-function init(){
-    
+function init() {
+
 
     fetchProjects()
 }
- function createProject(){
- 
-     var counter = JSON.parse(window.localStorage.getItem("counter")) || 0;
-     
+function createProject() {
 
-     
-     
-     counter = parseInt(counter);
-     counter++;
-     window.localStorage.setItem("counter",JSON.stringify(counter));
-     
-     console.log(counter);
-     
-     if(counter<7){
-        
+    var projects = JSON.parse(window.localStorage.getItem("projects")) || [];
+
+    var counter = JSON.parse(window.localStorage.getItem("counter")) || 0;
+
+
+
+
+    counter = parseInt(counter);
+    counter++;
+    window.localStorage.setItem("counter", JSON.stringify(counter));
+
+    console.log(counter);
+
+    if (counter < 7) {
+
         newProject.style.visibility = "hidden";
         newProject.style.opacity = 0;
-        
+
         /*Saves and publish local storage value*/
-        var projectInput = {
-            name: input.value,
-            date: "2020-03-10"
-        };
-        if(projectInput){
-            localStorage.setItem(`project${window.localStorage.length}`, JSON.stringify(projectInput));
-            
+        var name = modalNameinput.value;
+        var date = modalDateinput.value;
+
+        var project = {
+            name, date
+        }
+
+        if (project) {
+            projects.push(project);
+            console.log(projects);
+            localStorage.setItem(`projects`, JSON.stringify(projects));
+
             fetchProjects();
-           
+
+        }
+    }
+
+
 }
-    }
-    
-     
-    }
-function fetchProjects(){
+
+
+
+function fetchProjects() {
     projectContainer.innerHTML = "";
-    for (var i = 1; i < window.localStorage.length; i++) {
-    var projects = JSON.parse(window.localStorage.getItem(`project${i}`)); 
-        
-    var projectElement = document.createElement("div");
-        projectElement.innerHTML = `${projects.name} <br> ${projects.date}`;
+    
+
+    var projects = JSON.parse(window.localStorage.getItem(`projects`)) || [];
+
+    for (var i = 0; i < projects.length; i++) {
+        var projectElement = document.createElement("div");
+        projectElement.innerHTML = `<p>${projects[i].name} <br> ${projects[i].date}</p>`;
         projectElement.classList.add(`div${i}`);
         projectElement.classList.add(`div-common`);
         projectContainer.appendChild(projectElement);
-        projectElement.style.backgroundColor="#333";
-    
- 
-        
-    
-        
-    
-                  
-            }
+
+
+        var colorArray = ["#333", "#444", "#343", "#121", "#111", "#345"];
+
+        var color = colorArray[i];
+
+
+
+
+        projectElement.style.backgroundColor = color;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
